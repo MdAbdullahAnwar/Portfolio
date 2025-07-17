88fc3eae5d5c536961cd6e136a414dc9cbbf0961
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-07-17 08:24:37 +0530

    Contact

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -53,6 +53,7 @@ const Navbar = () => {
     { id: "experience", label: "Experience" },
     { id: "work", label: "Projects" },
     { id: "education", label: "Education" },
+    { id: "contact", label: "Contact"},
   ];
 
   return (

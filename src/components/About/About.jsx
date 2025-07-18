c533a387ae06ba295518d8fe98d252e41af34a89
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-07-19 03:09:51 +0530

    Photo Updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Typewriter } from "react-simple-typewriter";
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/Profile3.jpeg';
+import profileImage from '../../assets/Profile.jpg';
 
 const About = () => {
   return (

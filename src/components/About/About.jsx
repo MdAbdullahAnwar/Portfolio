7b1c16d80967c7b02c61c1bd61f905f3b5f6103f
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-07-19 03:00:05 +0530

    CV Uploaded

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Typewriter } from "react-simple-typewriter";
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/profile2.png';
+import profileImage from '../../assets/Profile3.jpeg';
 
 const About = () => {
   return (
@@ -55,7 +55,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/"
+            href="https://drive.google.com/file/d/1KILjBjPv_iFa6jnisFq3K1xAhDpbF5Zt/view"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"

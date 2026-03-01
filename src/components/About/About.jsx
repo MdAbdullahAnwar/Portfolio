39ce96742ba6482fe113c39aaccb788397802ab4
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2026-03-01 17:50:21 +0530

    Image section changed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Typewriter } from "react-simple-typewriter";
 import Tilt from "react-parallax-tilt";
-import profileImage from "../../assets/Profile.jpeg";
+import profileImage from "../../assets/Profile2.png";
 
 const About = () => {
   return (
